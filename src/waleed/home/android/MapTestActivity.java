266ddfbc91ab9d4ca32a2d2a473652c2298ce6d5
waleed hassan <waleedot2003@gmail.com>
2011-06-07 22:36:49 +0300

    adding zoom controls

--- src/waleed/home/android/MapTestActivity.java
+++ src/waleed/home/android/MapTestActivity.java
@@ -1,20 +1,24 @@
 package waleed.home.android;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 public class MapTestActivity extends MapActivity {
     private MapView mymap ;
     private static final boolean DISPLAYZOOMONMAP = true ; 
+    private MapController mController ;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         mymap = (MapView)findViewById(R.id.mapView);
-        mymap.setBuiltInZoomControls(DISPLAYZOOMONMAP);
+        mController = mymap.getController();        
+        mymap.setBuiltInZoomControls(DISPLAYZOOMONMAP);        
     }
 
     @Override
@@ -22,4 +26,20 @@ public class MapTestActivity extends MapActivity {
         // TODO Auto-generated method stub
         return false;
     }
+    /*
+     * Just for proof of concept that you can zoom In/Out with your own 
+     * action. you can test it in emulator 
+     * you can choose another action instead of KeyDown
+     * */
+    /*
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if(keyCode == KeyEvent.KEYCODE_1)
+            mController.zoomIn();
+        if(keyCode == KeyEvent.KEYCODE_3)
+            mController.zoomOut();
+        return super.onKeyDown(keyCode, event);
+    }
+    */
 }
